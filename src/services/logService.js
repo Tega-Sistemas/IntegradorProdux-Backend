874804eb32add63a32306d07e7f833b6b013c860
Pedro Zampiroli <73896961+pedrozampiroli@users.noreply.github.com>
2024-11-21 12:16:43 -0300

    remoção de comentários

--- src/services/logService.js
+++ src/services/logService.js
@@ -1,19 +1,10 @@
 import { v4 as uuidv4, v4 } from 'uuid';
 import Log from '../models/Log.js';
 
-// Função para registrar um log
 async function registrarLog(level, message) {
     try {
         let uuid = uuidv4();
-        console.log(uuid);
 
-        // Limitar o tamanho da mensagem
-        const maxMessageLength = 1024; // Tamanho máximo permitido para a mensagem
-        if (message.length > maxMessageLength) {
-            message = message.substring(0, maxMessageLength); // Trunca a mensagem
-        }
-
-        // Registrar o log na tabela 'logs'
         await Log.query().insert({
             id: uuid,
             level,
@@ -29,17 +20,14 @@ async function registrarLog(level, message) {
 export { registrarLog };
 
 
-// Função para registrar um log de INFO
 async function logInfo(message) {
     await registrarLog('INFO', message);
 }
 
-// Função para registrar um log de SUCESSO
 async function logSucesso(message) {
     await registrarLog('SUCESSO', message);
 }
 
-// Função para registrar um log de ERRO
 async function logErro(message, stackTrace) {
     await registrarLog('ERRO', message, stackTrace);
 }
