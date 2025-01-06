23ddd9b7875ad00ad5dcbe988ed3c90751b9a134
Renato Yoiti <renatoyoiti@teagsistemas.com.br>
2025-01-06 10:23:53 -0300

    changed default ip for knexfile

--- src/config/knexfile.js
+++ src/config/knexfile.js
@@ -7,7 +7,7 @@ export default {
   development: {
     client: 'mysql2',
     connection: {
-      host: process.env.DB_HOST || '192.168.100.125',
+      host: process.env.DB_HOST || '192.168.100.30',
       port: process.env.DB_PORT || 3306,
       user: process.env.DB_USER || 'root',
       password: process.env.DB_PASSWORD || 'f4c1l',
